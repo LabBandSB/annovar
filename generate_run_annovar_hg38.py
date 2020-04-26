03f7b86de24c263964de8bd76f968eeb4745da62
asmo <askhat.molkenov@gmail.com>
2020-04-26 13:44:50 +0600

    sh generation files on python

---
+++ generate_run_annovar_hg38.py
@@ -0,0 +1,73 @@
+import argparse
+import os
+
+table_annovar = "perl /home/PublicData/annovar_src/annovar_20190101/table_annovar.pl"
+annovar_db_folder = "/home/PublicData/annovar_src/annovar_20190101/humandb"
+
+db_gene = [
+    "refGene",
+    "knownGene",
+    "ensGene",
+]
+
+db_filter = [
+    "dbnsfp35c",
+    "dbscsnv11",
+    "intervar_20180118",
+    "cosmic70",
+    "esp6500siv2_ea",
+    "esp6500siv2_aa",
+    "esp6500siv2_all",
+    "exac03",
+    "exac03nontcga",
+    "exac03nonpsych",
+    "gene4denovo201907",
+    "gnomad211_exome",
+    "gnomad211_genome",
+    "kaviar_20150923",
+    "hrcr1",
+    "abraom",
+    "1000g2015aug",
+    "gme",
+    "mcap",
+    "revel",
+    "avsnp150",
+    "nci60",
+    "clinvar_20200316",
+    "regsnpintron",
+]
+
+protocol = ",".join(db_gene + db_filter)
+operation = ",".join(["g"] * len(db_gene) + ["f"] * len(db_filter))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--vcf", default=None, required=True)
+    args = parser.parse_args()
+
+    input_file = args.vcf
+    input_file_prefix, _ = os.path.splitext(input_file)
+    output_file = input_file_prefix + ".FINAL.annovar"
+
+    cmd = f"""
+        {table_annovar} \
+        {input_file} \
+        {annovar_db_folder} \
+        --protocol {protocol} \
+        --operation {operation}  \
+        --outfile {output_file} \
+        --buildver hg38 \
+        --remove \
+        --otherinfo \
+        --onetranscript \
+        --nastring '.' \
+        --vcfinput
+    """
+    cmd_file = input_file_prefix + '.RUN_ANNOVAR_hg38.sh'
+    with open (cmd_file, "w") as f:
+        f.write(cmd)
+
+
+if __name__ == '__main__':
+    main()
---
+++ generate_run_download_annovar_hg38.py
@@ -0,0 +1,53 @@
+db_gene = [
+    "refGene",
+    "knownGene",
+    "ensGene",
+]
+
+db_filter = [
+    "dbnsfp35c",
+    "dbscsnv11",
+    "intervar_20180118",
+    "cosmic70",
+    "esp6500siv2_ea",
+    "esp6500siv2_aa",
+    "esp6500siv2_all",
+    "exac03",
+    "exac03nontcga",
+    "exac03nonpsych",
+    "gene4denovo201907",
+    "gnomad211_exome",
+    "gnomad211_genome",
+    "kaviar_20150923",
+    "hrcr1",
+    "abraom",
+    "1000g2015aug",
+    "gme",
+    "mcap",
+    "revel",
+    "avsnp150",
+    "nci60",
+    "clinvar_20200316",
+    "regsnpintron",
+]
+
+db_list = db_gene + db_filter
+log_file = "download_annovar_hg38.completed.log"
+
+cmd_file = "run_download_annovar_hg38.sh"
+with open(cmd_file, "w") as f:
+    new_line = f"""
+export PATH=/home/PublicData/annovar_src/annovar_20190101:$PATH
+cd /home/PublicData/annovar_src/annovar_20190101
+touch {log_file}
+\n
+"""
+    f.write(new_line)
+    for db in db_list:
+        is_db_not_in_log = f"""[[ -z $( grep {db} {log_file} ) ]]"""
+        cmd = f"""annotate_variation.pl -buildver hg38 -downdb -webfrom annovar {db} humandb/"""
+        add_db_to_log = f"""echo {db} &>> {log_file}"""
+        new_line = f"""{is_db_not_in_log} && {cmd} && {add_db_to_log}\n"""
+        f.write(new_line)
+
+print(f"# bash {cmd_file}")
